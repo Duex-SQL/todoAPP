5ce6d38a2ea1852d1531e21a15ca5f3046a6ff08
Sql13525482154 <duex0702@gmail.com>
2021-02-20 18:52:43 +0800

    finally

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-bar title="TODO" lef="bars" right ="search"/>
+    <avatar/>
+    <gradient/>
+    <todo-list/>
+    <todo-detail/>
+    <todo-editing/>
+    <floating-button/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// 一个.表示当前目录 ..表示当前目录的上一级
+import AppBar from '../components/AppBar.vue'
+import Avatar from '../components/Avatar.vue'
+import Gradient from '../components/Gradient.vue'
+import TodoList from '../components/TodoList.vue'
+import TodoDetail from '../components/TodoDetail.vue'
+import TodoEditing from '../components/TodoEditing.vue'
+import FloatingButton from '../components/FloatingButton.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    AppBar,
+    Avatar,
+    Gradient,
+    TodoList,
+    TodoDetail,
+    TodoEditing,
+    FloatingButton
   }
 }
 </script>
+
+<style>
+  .home {
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    color: white;
+  }
+</style>
+
